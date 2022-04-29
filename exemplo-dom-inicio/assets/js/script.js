05d8240cc505635ef207af54e33e9c5af1092177
Farrush <rcarrenhot@gmail.com>
2022-04-29 09:40:09 -0300

    First Commit

--- exemplo-dom-inicio/assets/js/script.js
+++ exemplo-dom-inicio/assets/js/script.js
@@ -1,26 +1,52 @@
-const novaTarefa = document.querySelector('[data-form-button]')
-
 const criarTarefa = (evento) =>{
     evento.preventDefault()
-    
-
     const lista = document.querySelector('[data-list]')
+
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-
     const tarefa = document.createElement('li')
 
-    
-    const conteudo = `<p class="content">${valor}</p>`;
-    
-    lista.innerHTML += conteudo
-    lista.appendChild(tarefa)
     tarefa.classList.add('task')
-    
-    input.value = ""
 
+    const conteudo = `<p class="content">${valor}</p>`
+    tarefa.innerHTML = conteudo
+    lista.appendChild(tarefa)
+    input.value = " "
+
+    tarefa.appendChild(botao())
+    tarefa.appendChild(remover())
+}
+const botao = () =>{
+    const botaoConclui = document.createElement('button')
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerHTML = 'concluir'
+    botaoConclui.addEventListener('click', concluirTarefa)
+    return botaoConclui
 }
 
+const concluirTarefa = (evento) =>{
+
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}
+
+const remover = () =>{
+    const botaoDelete = document.createElement('button')
+    botaoDelete.classList.add('delete-button')
+    botaoDelete.innerHTML = 'deletar'
+    botaoDelete.addEventListener('click', removerTarefa)
+    return botaoDelete
+}
+const removerTarefa = (evento) => {
+    const botaoDelete = evento.target
+    const tarefaCompleta = botaoDelete.parentElement
+    tarefaCompleta.remove()
+    return botaoDelete
+}
+const novaTarefa = document.querySelector('[data-form-button]')
+
+
 novaTarefa.addEventListener('click',(criarTarefa))
-
-
